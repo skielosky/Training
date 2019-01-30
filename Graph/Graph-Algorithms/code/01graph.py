4e807e55be9eae26443d6e93abd4af2e32183b17
Ezequiel Aguilar <ezequiel.aguilarg@gmail.com>
2019-01-30 10:03:05 -0500

    Chapter 01

---
+++ Graph/Graph-Algorithms/code/01graph.py
@@ -0,0 +1,33 @@
+from pyspark.sql.types import *
+from graphframes import *
+
+def create_transport_graph():
+    node_fields = [
+        StructField("id", StringType(), True),
+        StructField("latitude", FloatType(), True),
+        StructField("longitude", FloatType(), True),
+        StructField("population", IntegerType(), True)
+    ]
+    nodes = spark.read.csv("data/transport-nodes.csv", header=True,
+                           schema=StructType(node_fields))
+
+    rels = spark.read.csv("data/transport-relationships.csv", header=True)
+    reversed_rels = rels.withColumn("newSrc", rels.dst) \
+        .withColumn("newDst", rels.src) \
+        .drop("dst", "src") \
+        .withColumnRenamed("newSrc", "src") \
+        .withColumnRenamed("newDst", "dst") \
+        .select("src", "dst", "relationship", "cost")
+
+    relationships = rels.union(reversed_rels)
+
+    return GraphFrame(nodes, relationships)
+
+g = create_transport_graph()
+
+g.vertices \
+    .filter("population > 100000 and population < 300000") \
+    .sort("population") \
+    .show()
+
+
